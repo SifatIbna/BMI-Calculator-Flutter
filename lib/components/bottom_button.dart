330171f83d3f5d5a2f3d2ba9d32650a5fa899a62
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-01-29 19:44:35 +0600

    component design complete

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../constants/constants.dart';
+
+class BottomButton extends StatelessWidget {
+  final Function onTap;
+  final String text;
+
+  BottomButton({this.onTap, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            text,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomColor,
+        margin: EdgeInsets.only(top: 10),
+        padding: EdgeInsets.only(bottom: 20),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
---
+++ lib/components/round_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  final IconData iconData;
+  final Function onPressed;
+
+  RoundIconButton({this.iconData, this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(iconData),
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      onPressed: onPressed,
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+    );
+  }
+}
